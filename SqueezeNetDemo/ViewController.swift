2abf0cf693d001d2e31738b8319013b569adfb5e
SorenHersomNielsen <hersom@ellekrattet.dk>
2021-11-28 10:45:45 +0100

    SqueezeNetDemo

--- SqueezeNetDemo/ViewController.swift
+++ SqueezeNetDemo/ViewController.swift
@@ -6,14 +6,71 @@
 //
 
 import UIKit
+import CoreML
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    
+    
+    @IBOutlet weak var ImageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBOutlet weak var Text: UILabel!
+    
+    @IBAction func ChooseImage(_ sender: Any) {
+        
+        let imagePickerController = UIImagePickerController()
+        imagePickerController.delegate = self
+        
+        let actionSheet = UIAlertController(title: "photo source", message: "Choose a source", preferredStyle: .actionSheet)
+        
+        actionSheet.addAction(UIAlertAction(title: "Camera", style: .default, handler: { (action:UIAlertAction) in
+            imagePickerController.sourceType = .camera
+            self.present(imagePickerController, animated: true, completion: nil)
+        }))
+        
+        actionSheet.addAction(UIAlertAction(title: "photo library", style: .default, handler: { (action:UIAlertAction) in
+            imagePickerController.sourceType = .photoLibrary
+            self.present(imagePickerController, animated: true, completion: nil)
+        }))
+        
+        actionSheet.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        self.present(actionSheet, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        let image = info[UIImagePickerController.InfoKey.originalImage] as! UIImage
+        
+        analyseImage(image: image)
+        ImageView.image = image
+        
+        picker.dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        picker.dismiss(animated: true, completion: nil)
+    }
+    
+    private func analyseImage(image: UIImage?) {
+        guard let buffer = image?.resize(size: CGSize(width: 227, height:227))?.getCVPixelBuffer() else {
+            return
+        }
+        
+        do {
+            let config = MLModelConfiguration()
+            let model = try SqueezeNet(configuration: config)
+            let input = SqueezeNetInput(image: buffer)
+            let output = try model.prediction(input: input)
+            let text = output.classLabel
+            Text.text = text
+        }
+        catch {
+            print(error.localizedDescription)
+        }
+    }
 }
 
